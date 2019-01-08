204fe23bfc9317c72cb4fb33761a123207d0b1c2
Minjun Kim <mjaything@gmail.com>
2019-01-08 17:28:39 +0100

    ft_strlcat done

---
+++ ft_strlcat.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: min-kim <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/01/07 19:29:19 by min-kim           #+#    #+#             */
+/*   Updated: 2019/01/08 17:28:25 by min-kim          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcat(char *dst, const char *src, size_t size)
+{
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	j = 0;
+	while (dst[i] != '\0' && i < size)
+		i++;
+	while (src[j] != '\0' && i < size - 1)
+		dst[i++] = src[j++];
+	if (size >= ft_strlen(dst))
+		dst[i] = '\0';
+	if (size < ft_strlen(dst))
+		return (ft_strlen(src) + size);
+	else
+		return (ft_strlen(src) + ft_strlen(dst));
+}
