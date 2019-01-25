105428f4bd30d571bf6367a233af8dfc258b9183
Minjun Kim <mjaything@gmail.com>
2019-01-25 14:32:15 +0100

    ft_strmapi 42 format

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -1,16 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: min-kim <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/01/25 14:31:36 by min-kim           #+#    #+#             */
+/*   Updated: 2019/01/25 14:31:52 by min-kim          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
-char    *ft_strmapi(char const *s, char (*f)(unsigned int, char))
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
-    char    *t;
-    int     i;
+	char	*t;
+	int		i;
 
-    t = ft_strnew(ft_strlen(s));
-    i = 0;
-    while (s[i] != '\0')
-    {
-        t[i] = f(i, s[i]);
-        i++;
-    }
-    return (t);
+	t = ft_strnew(ft_strlen(s));
+	i = 0;
+	while (s[i] != '\0')
+	{
+		t[i] = f(i, s[i]);
+		i++;
+	}
+	return (t);
 }
