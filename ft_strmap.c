637ead06ef4be27f54391f5fd57e23d892b1b758
Minjun Kim <mjaything@gmail.com>
2019-01-25 14:31:16 +0100

    ft_strmap 42 format

--- ft_strmap.c
+++ ft_strmap.c
@@ -1,16 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: min-kim <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/01/25 14:29:56 by min-kim           #+#    #+#             */
+/*   Updated: 2019/01/25 14:30:28 by min-kim          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
-char    *ft_strmap(char const *s, char (*f)(char))
+char	*ft_strmap(char const *s, char (*f)(char))
 {
-    char    const   *t;
-    int             i;
+	char const	*t;
+	int			i;
 
-    t = ft_strnew(ft_strlen(s));
-    i = 0;
-    while (s[i] != '\0')
-    {
-        t[i] = f(s[i]);
-        i++;
-    }
-    return (t);
+	t = ft_strnew(ft_strlen(s));
+	i = 0;
+	while (s[i] != '\0')
+	{
+		t[i] = f(s[i]);
+		i++;
+	}
+	return (t);
 }
