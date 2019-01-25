1b29c5b0ce28f19003d660034a8b0742bb760aad
Minjun Kim <mjaything@gmail.com>
2019-01-25 14:41:48 +0100

    ft_strsub norminette

--- ft_strsub.c
+++ ft_strsub.c
@@ -1,20 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strsub.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: min-kim <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/01/25 14:41:26 by min-kim           #+#    #+#             */
+/*   Updated: 2019/01/25 14:41:35 by min-kim          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
-char    *ft_strsub(char const *s, unsigned int start, size_t len)
+char	*ft_strsub(char const *s, unsigned int start, size_t len)
 {
-    char    *sub;
-    size_t  i;
+	char    *sub;
+	size_t  i;
 
-    if (!s)
-        return (NULL);
-    sub = ft_strnew(len);
-    if (!sub)
-        return (NULL);
-    i = 0;
-    while (i < len)
-    {
-        sub[i] = s[start + i];
-        i++;
-    }
-    return (sub);
+	if (!s)
+		return (NULL);
+	sub = ft_strnew(len);
+	if (!sub)
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		sub[i] = s[start + i];
+		i++;
+	}
+	return (sub);
 }
