466fc7d936ba88c1ef03480509a25812c484107b
Minjun Kim <mjaything@gmail.com>
2019-01-25 18:04:56 +0100

    ft_strsplit done

---
+++ ft_strsplit.c
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strsplit.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: min-kim <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/01/25 16:57:08 by min-kim           #+#    #+#             */
+/*   Updated: 2019/01/25 18:04:43 by min-kim          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int		wordcount(char const *str, char c)
+{
+	int	count;
+	int	i;
+
+	count = 0;
+	i = 0;
+	while (str[i] != '\0')
+	{
+		while (str[i] == c)
+			i++;
+		if (str[i] != c && str[i] != '\0')
+			count++;
+		while (str[i] != c && str[i] != '\0')
+			i++;
+	}
+	return (count);
+}
+
+char	**ft_strsplit(char const *s, char c)
+{
+	int		i;
+	int		j;
+	int		k;
+	char	**t;
+
+	i = 0;
+	j = 0;
+	if (!s || !(t = (char **)malloc(sizeof(*t) * (wordcount(s, c) + 1))))
+		return (NULL);
+	while (i < wordcount(s, c))
+	{
+		k = 0;
+		while (s[j] == c)
+			j++;
+		while (s[j] != c && s[j] != '\0')
+			t[i][k++] = s[j++];
+		t[i][k] = '\0';
+		i++;
+	}
+	t[i] = '\0';
+	return (t);
+}
