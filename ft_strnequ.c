c7fc8c4bf2f60695b7629db79baee944bce723ff
Minjun Kim <mjaything@gmail.com>
2019-01-25 14:41:02 +0100

    ft_strnequ norminette

--- ft_strnequ.c
+++ ft_strnequ.c
@@ -1,6 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnequ.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: min-kim <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/01/25 14:40:39 by min-kim           #+#    #+#             */
+/*   Updated: 2019/01/25 14:40:45 by min-kim          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
-int ft_strnequ(char const *s1, char const *s2, size_t n)
+int	ft_strnequ(char const *s1, char const *s2, size_t n)
 {
-    return (ft_strncmp(s1, s2, n) ? 0 : 1);
+	return (ft_strncmp(s1, s2, n) ? 0 : 1);
 }
