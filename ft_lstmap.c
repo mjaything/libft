1676192800be862658f3babc3f21747bebd3f60c
Minjun Kim <mjaything@gmail.com>
2019-01-27 15:02:19 +0100

    ft_lstmap draft

---
+++ ft_lstmap.c
@@ -0,0 +1,23 @@
+#include "libft.h"
+
+t_list  *ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
+{
+    t_list  *fresh;
+    t_list  *tmp;
+
+    if (!lst)
+        return (NULL);
+    tmp = f(lst);
+    fresh = tmp;
+    while ((*lst).next)
+    {
+        lst = (*lst).next;
+        if (!((*tmp).next = f(lst)))
+        {
+            free((*tmp).next);
+            return (NULL);
+        }
+        tmp = (*tmp).next;
+    }
+    return (fresh);
+}
