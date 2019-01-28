fc5561eb184821c941e9e30dbd20a7c0eea6386f
Minjun Kim <mjaything@gmail.com>
2019-01-28 15:08:36 +0100

    ft_lstmap norm

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -1,23 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: min-kim <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/01/28 15:07:40 by min-kim           #+#    #+#             */
+/*   Updated: 2019/01/28 15:07:56 by min-kim          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
-t_list  *ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
+t_list	*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
 {
-    t_list  *fresh;
-    t_list  *tmp;
+	t_list	*fresh;
+	t_list	*tmp;
 
-    if (!lst)
-        return (NULL);
-    tmp = f(lst);
-    fresh = tmp;
-    while ((*lst).next)
-    {
-        lst = (*lst).next;
-        if (!((*tmp).next = f(lst)))
-        {
-            free((*tmp).next);
-            return (NULL);
-        }
-        tmp = (*tmp).next;
-    }
-    return (fresh);
+	if (!lst)
+		return (NULL);
+	tmp = f(lst);
+	fresh = tmp;
+	while ((*lst).next)
+	{
+		lst = (*lst).next;
+		if (!((*tmp).next = f(lst)))
+		{
+			free((*tmp).next);
+			return (NULL);
+		}
+		tmp = (*tmp).next;
+	}
+	return (fresh);
 }
