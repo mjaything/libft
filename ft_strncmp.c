61cf354d67138b2a8881157a65660b6b35467760
Minjun Kim <mjaything@gmail.com>
2019-01-29 16:48:04 +0100

    ft_strncmp fixed

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: min-kim <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/09 16:03:36 by min-kim           #+#    #+#             */
-/*   Updated: 2019/01/28 16:01:12 by min-kim          ###   ########.fr       */
+/*   Updated: 2019/01/29 16:47:54 by min-kim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,12 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	while (n > 0 && *s1 != '\0' && *s1 == *s2)
-	{
-		s1++;
-		s2++;
-		n--;
-	}
-	return ((unsigned char)*s1 - (unsigned char)*s2);
+	size_t	i;
+
+	i = 0;
+	while ((s1[i] == s2[i]) && (i < n) && s1[i])
+		i++;
+	if (i == n)
+		return (0);
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
