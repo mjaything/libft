2a6b815d470a0b5553a1cb115f98ad8cbe6f2099
Minjun Kim <mjaything@gmail.com>
2019-01-29 23:25:57 +0100

    ft_strlcat fixed

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: min-kim <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/07 19:29:19 by min-kim           #+#    #+#             */
-/*   Updated: 2019/01/08 17:28:25 by min-kim          ###   ########.fr       */
+/*   Updated: 2019/01/29 23:18:42 by min-kim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,18 @@
 size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
 	size_t	i;
-	size_t	j;
+	size_t	len;
 
-	i = 0;
-	j = 0;
-	while (dst[i] != '\0' && i < size)
-		i++;
-	while (src[j] != '\0' && i < size - 1)
-		dst[i++] = src[j++];
-	if (size >= ft_strlen(dst))
-		dst[i] = '\0';
-	if (size < ft_strlen(dst))
-		return (ft_strlen(src) + size);
-	else
-		return (ft_strlen(src) + ft_strlen(dst));
+	len = 0;
+	while (dst[len] && len < size)
+		len++;
+	i = len;
+	while (src[len - i] && len + 1 < size)
+	{
+		dst[len] = src[len - i];
+		len++;
+	}
+	if (i < size)
+		dst[len] = '\0';
+	return (i + ft_strlen(src));
 }
