00908a58a5640f6ac20389dee27ff014ed7612ca
Minjun Kim <mjaything@gmail.com>
2019-01-29 17:01:48 +0100

    ft_atoi fixed

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: min-kim <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/28 16:03:45 by min-kim           #+#    #+#             */
-/*   Updated: 2019/01/28 19:39:00 by min-kim          ###   ########.fr       */
+/*   Updated: 2019/01/29 17:01:40 by min-kim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,15 +20,15 @@ int	ft_atoi(const char *str)
 
 	i = 0;
 	number = 0;
-	sign = 0;
+	sign = 1;
+	while (str[i] == ' ' || (str[i] >= '\t' && str[i] <= '\r'))
+		i++;
 	if (str[i] == '-')
 	{
 		sign = -1;
 		i++;
 	}
 	else if (str[i] == '+')
-		i++;
-	while (str[i] == ' ' || (str[i] >= '\t' && str[i] <= '\r'))
 		i++;
 	while (str[i] != '\0')
 	{
