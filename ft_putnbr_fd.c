1149c4683523496da4cdc88653a79c7567a38a51
Minjun Kim <mjaything@gmail.com>
2019-01-29 22:42:55 +0100

    ft_putnbr_fd fixed

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,26 +6,29 @@
 /*   By: min-kim <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/09 18:19:03 by min-kim           #+#    #+#             */
-/*   Updated: 2019/01/09 18:23:07 by min-kim          ###   ########.fr       */
+/*   Updated: 2019/01/29 18:56:32 by min-kim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_putnbr_fd(int nb, int fd)
+void	ft_putnbr_fd(int n, int fd)
 {
-	if (nb == -2147483648)
+	if (n == -2147483648)
 		ft_putstr_fd("-2147483648", fd);
-	if (nb < 0)
-	{
-		nb = -nb;
-		ft_putchar_fd('-', fd);
-	}
-	if (nb >= 10)
-	{
-		ft_putnbr_fd(nb / 10, fd);
-		ft_putnbr_fd(nb % 10, fd);
-	}
 	else
-		ft_putchar_fd(nb + '0', fd);
+	{
+		if (n < 0)
+		{
+			n = -n;
+			ft_putchar_fd('-', fd);
+		}
+		if (n >= 10)
+		{
+			ft_putnbr_fd(n / 10, fd);
+			ft_putnbr_fd(n % 10, fd);
+		}
+		else
+			ft_putchar_fd(n + '0', fd);
+	}
 }
