c4018bd9cb9969ecd418185086594740a533ad71
Minjun Kim <mjaything@gmail.com>
2019-01-29 22:42:18 +0100

    ft_memcpy fixed

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,22 +6,22 @@
 /*   By: min-kim <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/10 14:35:30 by min-kim           #+#    #+#             */
-/*   Updated: 2019/01/10 14:43:30 by min-kim          ###   ########.fr       */
+/*   Updated: 2019/01/29 19:19:43 by min-kim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memcpy(void *dest, const void *src, size_t n)
+void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
 	int i;
 
 	i = 0;
 	while (n > 0)
 	{
-		((unsigned char *)dest)[i] = ((unsigned char *)src)[i];
+		((unsigned char *)dst)[i] = ((unsigned char *)src)[i];
 		i++;
 		n--;
 	}
-	return (dest);
+	return (dst);
 }
