c23c4dde89edf8b01e984629c4342b35b0c79983
Minjun Kim <mjaything@gmail.com>
2019-01-29 22:44:06 +0100

    ft_strtrim fixed

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: min-kim <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/25 14:46:54 by min-kim           #+#    #+#             */
-/*   Updated: 2019/01/25 16:55:32 by min-kim          ###   ########.fr       */
+/*   Updated: 2019/01/29 22:40:49 by min-kim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,15 @@ char	*ft_strtrim(char const *s)
 	int	start;
 	int	end;
 
+	if (!s)
+		return (NULL);
 	start = 0;
 	end = ft_strlen(s) - 1;
-	if (start == end + 1)
-		return (ft_strnew(0));
 	while (s[start] == ' ' || s[start] == '\t' || s[start] == '\n')
 		start++;
+	if (start == end + 1)
+		return (ft_strnew(0));
 	while (s[end] == ' ' || s[end] == '\t' || s[end] == '\n')
 		end--;
-	return (ft_strsub(s, start, end - start + 1));
+	return (ft_strsub(s, (unsigned int)start, (unsigned int)(end - start + 1)));
 }
