8561d7519480b671952fb0c97385a7529a642270
Minjun Kim <mjaything@gmail.com>
2019-01-30 14:28:20 +0100

    ft_memmove norm

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: min-kim <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/28 15:12:31 by min-kim           #+#    #+#             */
-/*   Updated: 2019/01/29 23:24:27 by min-kim          ###   ########.fr       */
+/*   Updated: 2019/01/30 14:26:17 by min-kim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,12 +22,12 @@ void	*ft_memmove(void *dst, const void *src, size_t len)
 		{
 			len--;
 			((unsigned char *)dst)[len] = ((unsigned char *)src)[len];
-		}	
+		}
 	else
 		while (i < len)
 		{
 			((unsigned char *)dst)[i] = ((unsigned char *)src)[i];
-			 i++;
+			i++;
 		}
 	return ((unsigned char *)dst);
 }
