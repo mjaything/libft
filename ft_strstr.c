0627a343ccd5a27be6a595be9690e7fc299c8a6c
Minjun Kim <mjaything@gmail.com>
2019-01-30 17:25:19 +0100

    ft_strstr fixed

--- ft_strstr.c
+++ ft_strstr.c
@@ -6,7 +6,7 @@
 /*   By: min-kim <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/08 18:05:50 by min-kim           #+#    #+#             */
-/*   Updated: 2019/01/29 16:14:32 by min-kim          ###   ########.fr       */
+/*   Updated: 2019/01/30 17:20:26 by min-kim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,21 @@
 
 char	*ft_strstr(const char *haystack, const char *needle)
 {
-	int i;
+	int	i;
+	int	j;
 
 	i = 0;
-	if (needle[i] == '\0')
-		return ((char *)haystack);
-	while ((*haystack != *needle) && (*haystack != '\0'))
-		haystack++;
-	if (*haystack == '\0')
-		return (NULL);
-	while ((haystack[i] == needle[i]) && (needle[i] != '\0'))
+	j = 0;
+	if (!needle[0])
+		return ((char *)&haystack[i]);
+	while (haystack[i])
+	{
+		while ((haystack[i + j] == needle[j]) && needle[j])
+			j++;
+		if (!needle[j])
+			return ((char *)&haystack[i]);
+		j = 0;
 		i++;
-	if (needle[i] == '\0')
-		return ((char *)haystack);
-	else
-		return (ft_strstr(haystack + 1, needle));
+	}
+	return (NULL);
 }
