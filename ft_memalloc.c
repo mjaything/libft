c0258ff35ef198960bf9c0bc910f079e9371ba53
Minjun Kim <mjaything@gmail.com>
2019-01-31 16:08:45 +0100

    ready to be corrected

--- ft_memalloc.c
+++ ft_memalloc.c
@@ -6,7 +6,7 @@
 /*   By: min-kim <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/15 14:07:39 by min-kim           #+#    #+#             */
-/*   Updated: 2019/01/28 15:42:34 by min-kim          ###   ########.fr       */
+/*   Updated: 2019/01/31 15:50:39 by min-kim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,14 @@
 
 void	*ft_memalloc(size_t size)
 {
-	void	*ptr;
+	unsigned char	*ptr;
 
-	ptr = malloc(size);
-	if (ptr == NULL)
-		return (NULL);
-	ft_bzero(ptr, size);
+	ptr = NULL;
+	if (size)
+	{
+		if (!(ptr = (unsigned char *)malloc(size)))
+			return (NULL);
+		ft_bzero(ptr, size);
+	}
 	return (ptr);
 }
